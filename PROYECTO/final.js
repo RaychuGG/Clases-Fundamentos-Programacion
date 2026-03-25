e13a802553f8f0ca315ad41fc5b77f1a2ded6ae9
RaychuGG <rechellleal@gmail.com>
2026-03-24 21:37:58 -0600

    Add files via upload

---
+++ PROYECTO/final.js
@@ -0,0 +1,23 @@
+const age = 30;
+const hasCuopon = true;
+const couponDiscount = 10;
+const hasStudentId = false;
+
+let price = 5000;
+let discount = 0
+
+if (age < 12 ){
+    discount = 100;
+} else{
+    if (age >= 65){
+        discount = 40;
+    }
+    if (hasStudentId && 25){
+        discount = 25;
+    }
+    if (hasCuopon && couponDiscount)
+        discount = couponDiscount;
+}
+price = basePrice - (basePrice * discount / 100);
+
+console.log("El precio de su tiquete es:," price);
